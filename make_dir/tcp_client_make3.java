88e277ea8401c8c18b2fa3c124ff53f174fc3976
Kuazk <allenz@m1-mac-allen.lan>
2023-04-13 09:40:37 -0500

    makefile update

---
+++ make_dir/tcp_client_make3.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.net.*;
+
+class TCPClientM3 {
+    public static void main(String argv[]) throws Exception {
+        String operRequest, operAns;
+		String joinRequest, termRequest;
+		String joinAns, termAns;
+
+		Socket clientSocket = new Socket("localhost", 8000);
+
+		// create output and input streams attached to the socket
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+        String name = "Jhon";
+        joinRequest = "JOIN_REQUEST|" + name;
+        outToServer.writeBytes(joinRequest + '\n');
+        
+        // get the ACK for the join request
+        joinAns = inFromServer.readLine();
+        
+        if (joinAns.equals("JOIN_ACK|" + name)) {
+            // if ACK received properly do nothing
+            System.out.println("Connection established");
+        }
+		
+		// establish request to make 3 math calculations
+        for (int i = 1; i<4; i++) {
+            String operation, operand1, operand2;
+            switch (i) {
+                case 1:
+                    operation = "ADD";
+                    operand1 = "4";
+                    operand2 = "5";
+                    break;
+                case 2:
+                    operation = "SUB";
+                    operand1 = "9";
+                    operand2 = "4";
+                    break;
+                case 3:
+                    operation = "MUL";
+                    operand1 = "5";
+                    operand2 = "3";
+                    break;
+                default:
+                    operation = "";
+                    operand1 = "";
+                    operand2 = "";
+                    break;
+            }
+            operRequest = "CALCULATION_REQUEST|" + operation + "|" + operand1 + "|" + operand2;
+            outToServer.writeBytes(operRequest + '\n');
+            // get answer from server and output it on the screen
+            operAns = inFromServer.readLine();
+            // if there was an error in the calculation print 'error'
+            System.out.println("Server response: " + operAns);
+            String[] res = operAns.split("\\|");
+            System.out.println("Answer from server: " + res[res.length-1]);
+        }
+
+      
+
+        
+		
+		// since client doesn't want to make any more requests, terminate connection
+		// make the termination request and send it to server
+		termRequest = "TERMINATION_REQUEST|" + name;
+		outToServer.writeBytes(termRequest + '\n');
+
+		// get the ACK for the termination request
+		termAns = inFromServer.readLine();
+		if (termAns.equals("TERMINATION_ACK|" + name)) {
+			// ACK received properly so terminate connection
+            System.out.println("Connection terminated");
+			clientSocket.close();
+		}
+		// if ACK not received then display an error
+		else {
+			System.out.println("Error: wasn't able to terminate connection");
+			System.exit(0);
+		}
+		
+		// close the socket
+		clientSocket.close();
+
+    }
+}
+
+	
---
+++ make_dir/tcp_server_make.java
@@ -0,0 +1,153 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.time.Duration;
+import java.time.Instant;
+
+class TCPServer {
+    
+    public static void main(String argv[]) throws Exception {
+        
+        // create a server socket that listens on port 6789
+        ServerSocket welcomeSocket = new ServerSocket(8000);
+        
+        while (true) {
+            
+            // wait for a client to connect
+            Socket connectionSocket = welcomeSocket.accept();
+            
+            // create a new thread to handle the client connection
+            Thread clientThread = new Thread(new ClientHandler(connectionSocket));
+            
+            // start the thread to handle the client connection
+            clientThread.start();
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+    
+    private Socket connectionSocket;
+    private String clientName;
+    private Instant joinTime;
+    private Duration duration;
+
+    
+    public ClientHandler(Socket connectionSocket) {
+        this.connectionSocket = connectionSocket;
+    }
+    
+    public void run() {
+        
+        String clientMessage;
+        String[] tokens;
+        
+        try {
+            
+            // create input and output streams for the client socket
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            
+            // loop to continuously handle client requests
+            while (!connectionSocket.isClosed()) {
+            // get the client's request
+                clientMessage = inFromClient.readLine();
+                if (clientMessage == null) {
+                    break;
+                }
+                // split the message into tokens using '|'
+                tokens = clientMessage.split("\\|");
+                
+                File logFile = new File("make_log.txt");
+                if (!logFile.exists()) {
+                    logFile.createNewFile();
+                }
+            
+                FileWriter fw = new FileWriter(logFile, true);
+                
+                // check the type of the message
+                if (tokens[0].equals("JOIN_REQUEST")) {
+                    handleJoinRequest(tokens, outToClient, fw, clientMessage);
+                } else if (tokens[0].equals("TERMINATION_REQUEST")) {
+                    handleTerminationRequest(tokens, outToClient, fw, clientMessage);
+                    // close the client socket
+                    connectionSocket.close();
+                } else if (tokens[0].equals("CALCULATION_REQUEST")) {
+                    handleCalculationRequest(tokens, outToClient, fw, clientMessage);
+                } else {
+                    // invalid message
+                    outToClient.writeBytes("ERROR\n");
+                }
+    
+                fw.flush();
+                fw.close();
+            }
+            
+        } catch (IOException e) {
+            // handle any network errors that occur
+            e.printStackTrace();
+        } finally {
+            try {
+                // close the client socket
+                connectionSocket.close();
+            } catch (IOException e) {
+                // handle any errors that occur while closing the socket
+                e.printStackTrace();
+            }
+        }
+    }
+    private void handleJoinRequest(String[] tokens, DataOutputStream outToClient, FileWriter fw, String clientMessage) throws IOException {
+        clientName = tokens[1];
+        outToClient.writeBytes("JOIN_ACK|" + clientName + "\n");
+        //start the timer
+        joinTime = Instant.now();
+        // write to log file
+        fw.write(clientName + ", " + joinTime + ", " + clientMessage+"\n");
+        System.out.println("New client joined: " + clientName);
+    }
+
+    private void handleTerminationRequest(String[] tokens, DataOutputStream outToClient, FileWriter fw, String clientMessage) throws IOException {
+        clientName = tokens[1];
+        outToClient.writeBytes("TERMINATION_ACK|" + clientName + "\n");
+        //end the timer
+        Instant endTime = Instant.now();
+        duration = Duration.between(joinTime, endTime);
+        fw.write(clientName + ", " + endTime + ", "+ duration.getSeconds()+" seconds" +", "+ clientMessage+"\n");
+        System.out.println("Client " + clientName + " terminated connection");
+    }
+
+    private void handleCalculationRequest(String[] tokens, DataOutputStream outToClient, FileWriter fw, String clientMessage) throws IOException {
+        String mathOperation = tokens[1];
+        int operand1 = Integer.parseInt(tokens[2]);
+        int operand2 = Integer.parseInt(tokens[3]);
+        int result = 0;
+        switch (mathOperation) {
+            case "ADD":
+                result = operand1 + operand2;
+                break;
+            case "SUB":
+                result = operand1 - operand2;
+                break;
+            case "MUL":
+                result = operand1 * operand2;
+                break;
+            case "DIV":
+                result = operand1 / operand2;
+                break;
+            default:
+                outToClient.writeBytes("CALCULATION_ERROR|" + mathOperation + "\n");
+                break;
+        }
+        outToClient.writeBytes("CALCULATION_ACK|" + mathOperation + "|" + operand1 + "|" + operand2 + "|" + result + "\n");
+    
+        Instant currTime = Instant.now();
+        duration = Duration.between(joinTime, currTime);
+        fw.write(clientName + ", " + currTime + ", " + duration.getSeconds() + " seconds" + ", " + String.join("|", tokens) + "\n");
+    
+        System.out.println("Calculation request received from " + clientName + ": " + mathOperation + "|" + operand1 + "|" + operand2);
+        System.out.println("Result sent to " + clientName + ": " + mathOperation + "|" + operand1 + "|" + operand2 + "|" + result);
+    }
+}
+   
+
+
